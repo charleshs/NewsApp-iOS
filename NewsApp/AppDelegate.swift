29b6909604e21b055fa3f06bb76a3d5455b5a34d
charleshs <charlous167@gmail.com>
2020-02-21 23:03:48 +0800

    Create Xcode project. Initialize application programmatically without storyboard.

---
+++ NewsApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Kai-Ta Hsieh on 2020/2/21.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ NewsApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  NewsApp
+//
+//  Created by Kai-Ta Hsieh on 2020/2/21.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
+
